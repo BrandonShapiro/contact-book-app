afb293edc606c650fb4160dec8ab97e44e7fd46c
Brandon Shapiro <brandonshapiro9@gmail.com>
2022-08-06 11:05:10 -0600

    Initial Commit

---
+++ Contact Book/Contact_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_BookApp.swift
+//  Contact Book
+//
+//  Created by Brandon Shapiro on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Contact_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
